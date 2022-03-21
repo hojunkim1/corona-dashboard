ad1f0ff4a10f6723be29f5587d4b1292c429c4ac
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2022-03-21 23:30:52 +0900

    add layout

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-from corona_data import CoronaData
+from dash import Dash, html
 
-print(CoronaData().make_timeline_df_by_country('Korea, South'))
+stylesheets = [
+    'https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css',
+    'https://fonts.googleapis.com/css2?family=Open+Sans&display=swap',
+]
+
+app = Dash(__name__, external_stylesheets=stylesheets)
+
+app.layout = html.Div(
+    children=[
+        html.Header(
+            children=[html.H1(children='Corona Dashboard', style={'font-size': '50px'})],
+            style={'textAlign': 'center', 'paddingTop': '50px', }
+        )
+    ],
+    style={
+        'fontFamily': 'Open Sans, sans-serif',
+        'minHeight': '100vh',
+        'backgroundColor': '#111111',
+        'color': 'white'
+    }
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
