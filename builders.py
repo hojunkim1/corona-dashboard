902cfde13b28d671525cf3f64901049118c56e2a
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2022-04-01 23:58:58 +0900

    add chart

---
+++ builders.py
@@ -0,0 +1,46 @@
+from dash import html
+
+
+def make_table(df):
+    return html.Table(
+        children=[
+            html.Thead(
+                children=[
+                    html.Tr(
+                        children=[
+                            html.Th(column_name.replace('_', ' '))
+                            for column_name in df.columns
+                        ],
+                        style={
+                            'display': 'grid',
+                            'gridTemplateColumns': 'repeat(4, 1fr)',
+                            'fontWeight': '600',
+                            'fontSize': 22,
+                        },
+                    )
+                ],
+                style={'display': 'block', 'marginBottom': 25},
+            ),
+            html.Tbody(
+                children=[
+                    html.Tr(
+                        children=[
+                            html.Td(
+                                children=[value_column],
+                                style={"textAlign": "center"},
+                            )
+                            for value_column in value
+                        ],
+                        style={
+                            'display': 'grid',
+                            'gridTemplateColumns': 'repeat(4, 1fr)',
+                            'border-top': '1px solid white',
+                            'padding': '30px 0px',
+                        },
+                    )
+                    for value in df.values
+                ],
+                style={'maxHeight': '50vh', 'display': 'block', 'overflow': 'scroll', },
+            )
+        ]
+    )
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from dash import Dash, html
 
+from builders import make_table
+from corona_data import CoronaData
+
+countries_df = CoronaData().make_daily_df_by_country()
+
 stylesheets = [
     'https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css',
-    'https://fonts.googleapis.com/css2?family=Open+Sans&display=swap',
+    'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap',
 ]
 
 app = Dash(__name__, external_stylesheets=stylesheets)
@@ -11,8 +16,17 @@ app.layout = html.Div(
     children=[
         html.Header(
             children=[html.H1(children='Corona Dashboard', style={'font-size': '50px'})],
-            style={'textAlign': 'center', 'paddingTop': '50px', }
-        )
+            style={"textAlign": "center", "paddingTop": "50px", "marginBottom": 100},
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        make_table(countries_df)
+                    ]
+                ),
+            ]
+        ),
     ],
     style={
         'fontFamily': 'Open Sans, sans-serif',
